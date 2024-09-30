244e2f2385cb67db73c4358e72207a91e0490c58
Ilya Vasilyev <vasilyev.i123@gmail.com>
2024-09-30 11:53:25 +0300

    update

--- bin/start_project.sh
+++ bin/start_project.sh
@@ -4,10 +4,11 @@ echo "ЗАПУСК ПРОЕКТА"
 cp $GITHUB_WORKSPACE/backend/.env.example $GITHUB_WORKSPACE/backend/.env
 sed -i~ '/^DB_ADDRESS=/s/=.*/=mongodb:\/\/root:example@mongo:27017\/weblarek?authSource=admin/' $GITHUB_WORKSPACE/backend/.env
 cp $GITHUB_WORKSPACE/frontend/.env.example $GITHUB_WORKSPACE/frontend/.env
-sed -i~ '/^VITE_API_ORIGIN=/s/=.*/=http:\/\/localhost\/api/' $GITHUB_WORKSPACE/backend/.env
+sed -i~ '/^VITE_API_ORIGIN=/s/=.*/=http:\/\/localhost\/api/' $GITHUB_WORKSPACE/frontend/.env
 cd $GITHUB_WORKSPACE
 docker compose up -d
 
+cat $GITHUB_WORKSPACE/backend/.env
 mongo --config /etc/mongod.conf
 docker exec -i $REPO-mongo-1 sh -c "mongoimport --authenticationDatabase admin --authenticationMechanism SCRAM-SHA-256 -u root -p example --drop -c users --jsonArray --uri=mongodb://localhost/weblarek?authSource=admin" < .dump/weblarek.users.json
 docker exec -i $REPO-mongo-1 sh -c "mongoimport --authenticationDatabase admin --authenticationMechanism SCRAM-SHA-256 -u root -p example --drop -c products --jsonArray --uri=mongodb://localhost/weblarek?authSource=admin" < .dump/weblarek.products.json
