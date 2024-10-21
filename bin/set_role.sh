496d097842d2123fbaa1c7cfe35c057944a9df69
Ilya Vasilyev <vasilyev.i123@gmail.com>
2024-10-21 13:22:31 +0300

    update

--- bin/set_role.sh
+++ bin/set_role.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -i fullstack-developer-canonicals-mongo-1 sh -c "mongosh weblarek --authenticationDatabase admin -u root -p example --eval 'db.users.updateOne({email: \"admin@mail.ru\"}, {\$set: {roles: [\"admin\"]}})'"
+docker exec -i $REPO-mongo-1 sh -c "mongosh weblarek --authenticationDatabase admin -u root -p example --eval 'db.users.updateOne({email: \"admin@mail.ru\"}, {\$set: {roles: [\"admin\"]}})'"
