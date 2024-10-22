34b0d6d011d9b122c597bdca71d1fa7701d8b5f1
Ilya Vasilyev <vasilyev.i123@gmail.com>
2024-10-22 13:48:26 +0300

    update

--- bin/run.sh
+++ bin/run.sh
@@ -8,12 +8,12 @@ echo "ЗАГРУЖАЕМ ИЗОБРАЖЕНИЯ ДЛЯ ТЕСТОВ"
 curl -o $DIR_TESTS/data/bimage.png https://getsamplefiles.com/download/png/sample-1.png
 curl -o $DIR_TESTS/data/mimage.png https://getsamplefiles.com/download/png/sample-2.png
 
+echo "ЗАПИСЬ ЛОГОВ"
+mkdir -p output
+docker logs -f $REPO-backend-1 > output/backend.log &
+
 echo "ЗАПУСК ТЕСТОВ"
 cd $DIR_TESTS || exit
 node ./prepare.js
 sh ./bin/set_role.sh
 npm run test
-
-echo "СОХРАНЕНИЕ ЛОГОВ"
-mkdir -p output
-docker logs $REPO-backend-1 > output/backend.log
